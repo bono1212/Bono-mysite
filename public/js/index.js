7d1e6f159cb653d5c86cfafba0c3f02f8df7587a
bono1212 <uyanga1212g@gmail.com>
2020-09-23 18:20:26 +0900

    gg

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,11 @@
 (function(){
 	var slides = [
 		{ id: 0, src: '../img/h2-slider-1.jpg', title: 'people1'},
-		{ id: 0, src: '../img/h2-slider-2.jpg', title: 'people2'},
-		{ id: 0, src: '../img/h2-slider-3.jpg', title: 'people3'},
+		{ id: 1, src: '../img/h2-slider-2.jpg', title: 'people2'},
+		{ id: 3, src: '../img/h2-slider-3.jpg', title: 'people3'},
 	];
 
-	var $banner = $(".header-wrapper .banner-wrap");
+	/* var $banner = $(".header-wrapper .banner-wrap");
 	var $slide = (".header-wrapper .slide");
 	var $btnPrev = (".header-wrapper .btn-prev");
 	var $btnNext = (".header-wrapper .btn-next");
@@ -19,7 +19,7 @@
 		for(i in slides) {
 
 		}
-	}
+	} */
 
 		
 
