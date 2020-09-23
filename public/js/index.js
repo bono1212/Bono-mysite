eb6bbbce84f7545b9b2a12f063cd292e071c8664
bono1212 <uyanga1212g@gmail.com>
2020-09-24 01:05:40 +0900

    gg

--- public/js/index.js
+++ public/js/index.js
@@ -5,25 +5,43 @@
 		{ id: 3, src: '../img/h2-slider-3.jpg', title: 'people3'},
 	];
 
-	/* var $banner = $(".header-wrapper .banner-wrap");
 	var $slide = (".header-wrapper .slide");
 	var $btnPrev = (".header-wrapper .btn-prev");
 	var $btnNext = (".header-wrapper .btn-next");
 	var $slides =[];
 	var idx= 0;
 	var lastIdx = slides.length - 1;
-	var interval;
 
 	function init() {
 		var html, i;
 		for(i in slides) {
-
+			html = '<div class="slide">';
+			html += '<img class="w-100" src="'+slides[i].src+'">';
+			html += '<h1>'+i+'</h1>';
+			html += '</div>';
+			$slides[i] = $(html);
 		}
-	} */
+	}
 
-		
+	function slideInit() {
+		$slide.html($slides[idx].clone());
+	}
 
+	$btnPrev.click(onPrev);
+	$btnNext.click(onNext);
 
+	function onPrev() {
+		idx = (idx == 0) ? lastIdx : idx - 1;
+		ani();
+	}
 
+	function onNext() {
+		idx = (idx == lastIdx) ? 0 : idx + 1;
+		ani();
+	}
 
+	function ani() {
+			$($slides[idx].clone()).appendTo($slide).stop().animate({"opacity": 1}, 500, slideInit);
+	}
+	init();
 })();
