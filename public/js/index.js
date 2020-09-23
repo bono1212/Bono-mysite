b614bf23f29150b8898300a68cdf75adc3e3c218
bono1212 <uyanga1212g@gmail.com>
2020-09-23 16:10:27 +0900

    jj

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+/* (function(){
+	var slides = [
+		{ id: 0, src: '../img/h2-slider-1.jpg', title: 'people'},
+		{ id: 0, src: '../img/h2-slider-2.jpg', title: 'people'},
+		{ id: 0, src: '../img/h2-slider-3.jpg', title: 'people'},
+	];
+})(); */
