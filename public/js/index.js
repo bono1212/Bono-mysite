50dc31e2a8f59765b4f65d274db0662bce235418
bono1212 <uyanga1212g@gmail.com>
2020-09-23 18:10:26 +0900

    ff

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,29 @@
-/* (function(){
+(function(){
 	var slides = [
-		{ id: 0, src: '../img/h2-slider-1.jpg', title: 'people'},
-		{ id: 0, src: '../img/h2-slider-2.jpg', title: 'people'},
-		{ id: 0, src: '../img/h2-slider-3.jpg', title: 'people'},
+		{ id: 0, src: '../img/h2-slider-1.jpg', title: 'people1'},
+		{ id: 0, src: '../img/h2-slider-2.jpg', title: 'people2'},
+		{ id: 0, src: '../img/h2-slider-3.jpg', title: 'people3'},
 	];
-})(); */
+
+	var $banner = $(".header-wrapper .banner-wrap");
+	var $slide = (".header-wrapper .slide");
+	var $btnPrev = (".header-wrapper .btn-prev");
+	var $btnNext = (".header-wrapper .btn-next");
+	var $slides =[];
+	var idx= 0;
+	var lastIdx = slides.length - 1;
+	var interval;
+
+	function init() {
+		var html, i;
+		for(i in slides) {
+
+		}
+	}
+
+		
+
+
+
+
+})();
