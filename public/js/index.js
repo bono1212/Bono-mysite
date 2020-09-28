7f0997ca7f269cb5852f0c7dbb0275e111905a58
bono1212 <uyanga1212g@gmail.com>
2020-09-28 23:42:39 +0900

    ff

--- public/js/index.js
+++ public/js/index.js
@@ -146,3 +146,15 @@ $(document).ready(function () {
 
 	init();
 })();
+
+
+
+var container = document.getElementById('map');
+var options = { 
+	center: new kakao.maps.LatLng(41.382051, 2.167046),
+	level: 10
+};
+
+
+
+var map = new kakao.maps.Map(container, options);
