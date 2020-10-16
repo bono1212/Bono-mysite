69ea8434c027bfc9f3acd8cedc13c4dc051483c3
bono1212 <uyanga1212g@gmail.com>
2020-10-16 17:09:00 +0900

    hh

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,46 @@ function onScroll() {
 	}
 }
 
+(function () {
+	var $slideWrap = $(".header-wrapper .slide-wrap");
+	var $btnPrev = $(".header-wrapper .btn-prev");
+	var $btnNext = $(".header-wrapper .btn-next");
+	var slides = $(".slide-wrap .slide");
+	var idx = 0;
+	var lastIdx = slides.length - 1;
+	var interval;
+
+	function init() {
+		slideInit();
+		interval = setInterval(onNext, 3000);
+	}
+
+	function slideInit() {
+		$(slides[idx]).clone().appendTo($slideWrap.empty());
+	}
+
+	$btnPrev.click(onPrev);
+	$btnNext.click(onNext);
+
+	function onPrev() {
+		idx = (idx == 0) ? lastIdx : idx - 1; 
+		ani();
+	}
+
+	function onNext() {
+		idx = (idx == lastIdx) ? 0 : idx + 1;
+		ani();
+	}
+
+	function ani() {
+		$(slides[idx]).clone().appendTo($slideWrap).css("opacity", 0).stop().animate({ "opacity": 1 }, 500, slideInit);
+	}
+
+	init();
+})();
+
+
+
 
 /* ********small-banner effect********** */
 $(".small-banner .box").on("mouseenter", function(){
